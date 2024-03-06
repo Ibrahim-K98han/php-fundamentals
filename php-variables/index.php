04bcfc387abe7d17cdc07b65eab16d4cc0756d37
Ibrahim Khan <aitvetibrahim@gmail.com>
2024-03-07 04:21:27 +0600

    php variable

---
+++ php-variables/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP Syntax</title>
+</head>
+<body>
+<div class='phpCoding'>
+    <section class="headerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+    <section class='mainContent'>
+        <?php
+            $a = 10;
+            $b = 10;
+            $c = $a + $b;
+            echo "Total Value : ".$c." OK fine";
+        ?>
+    </section>
+    <section class="footerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+</div>
+   
+</body>
+</html>
