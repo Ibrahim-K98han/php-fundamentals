c0d1d3a1de5a66b4f7d65a763211e9d6d8c47869
Ibrahim Khan <aitvetibrahim@gmail.com>
2024-03-06 23:46:36 +0600

    php syntax done

--- php-syntax/index.php
+++ php-syntax/index.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>PHP Syntax</title>
 </head>
 <body>
-    
+<div class='phpCoding'>
+    <section class="headerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+    <section class='mainContent'>
+        <?php
+            echo 'hello';
+        ?>
+    </section>
+    <section class="footerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+</div>
+   
 </body>
 </html>
