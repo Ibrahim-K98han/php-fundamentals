4204e16603e7d578e66cc13347f2f8b0c5faab32
Ibrahim Khan <aitvetibrahim@gmail.com>
2024-03-07 20:39:44 +0600

    php string

---
+++ php-string/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP Syntax</title>
+</head>
+<body>
+<div class='phpCoding'>
+    <section class="headerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+    <section class='mainContent'>
+        <?php
+           $x = 'PHP is nice';
+            // echo strlen($x); length
+            // echo str_word_count($x); word count
+            // echo strrev($x); revised
+            // echo strpos($x, "nice"); position
+            // echo str_replace("PHP","JAVA",$x); replace
+        ?>
+    </section>
+    <section class="footerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+</div>
+   
+</body>
+</html>
