594fc0afd97a3391ef6fa3f6e929c3af105e22f2
Ibrahim Khan <aitvetibrahim@gmail.com>
2024-03-07 20:31:42 +0600

    php datatype

---
+++ php-data-type/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP Syntax</title>
+</head>
+<body>
+<div class='phpCoding'>
+    <section class="headerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+    <section class='mainContent'>
+        1. string
+        2.Integer
+        3.Float
+        4.Boolean
+        5.Array
+        6.Object
+        7.Null
+        8.Resource
+        <br>
+        <hr>
+        <?php
+            // 1. String
+            // $x = 'PHP Data Types';
+            // echo $x;
+
+            // 2. Integer
+            // $x = 28;
+            // echo $x;
+            // echo "<br>";
+            // var_dump($x);
+
+            // 3.Float
+            // $x = 28.3;
+            // var_dump($x);
+
+            // 4.Boolean
+            // $x = true;
+            // var_dump($x);
+
+            // 5.Array
+            // $x = array("Ibrahim", "Hasan", "Tomal");
+            // var_dump($x);
+
+            // 6.Object
+            // Class Student{
+            //     function department(){
+            //         return 'CSE';
+            //     }
+            //     function details(){
+            //         echo $this->department();
+            //     }
+            // }
+
+            // $st =  new Student();
+            // $st->details();
+
+            // 7.Null
+            // $x = "PHP";
+            // $x = null;
+            // var_dump($x);
+            
+
+        ?>
+    </section>
+    <section class="footerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+</div>
+   
+</body>
+</html>
