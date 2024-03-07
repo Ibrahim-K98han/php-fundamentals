4332b05c86526040f7c93a43e4ab6660c1883bf5
Ibrahim Khan <aitvetibrahim@gmail.com>
2024-03-07 21:13:25 +0600

    php constants

---
+++ php-constants/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP Syntax</title>
+</head>
+<body>
+<div class='phpCoding'>
+    <section class="headerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+    <section class='mainContent'>
+        <?php
+            define("VALUE", "I am Learning PHP");
+            
+            function learPHP(){
+                return VALUE;
+            }
+           echo learPHP();
+            
+        ?>
+    </section>
+    <section class="footerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+</div>
+   
+</body>
+</html>
