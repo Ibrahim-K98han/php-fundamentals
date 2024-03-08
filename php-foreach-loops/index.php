50034c6aa76d7435d57be09811ad6ca616c5dbdb
Ibrahim Khan <aitvetibrahim@gmail.com>
2024-03-08 07:25:46 +0600

    php foreach loop

---
+++ php-foreach-loops/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP Syntax</title>
+</head>
+<body>
+<div class='phpCoding'>
+    <section class="headerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+    <section class='mainContent'>
+        <?php
+            $colors = array(
+                "Blue",
+                "Red",
+                "Green",
+                "Orange"
+            );
+            foreach($colors as $color){
+                echo "$color <br>";
+            }
+        ?>
+    </section>
+    <section class="footerOption">
+        <h2><?php echo "PHP Fundamentals"; ?></h2>
+    </section>
+</div>
+   
+</body>
+</html>
